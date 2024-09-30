7eca7a95b0812d94d860c5957bb088b8c5e53f2e
RosarioManny <rosario.emm47@gmail.com>
2024-09-30 11:42:12 -0400

    Signout

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-
-module.exports = router;
+const User = require("../models/user.js");
+const bcrypt = require('bcrypt')
+const MongoStore = require("connect-mongo");
 
 // #4 This folder is going to handle the routes.
 
@@ -9,4 +10,83 @@ router.get("/sign-up", (req, res) => {
     res.render("auth/sign-up.ejs");
 });
   
-// #4a Created route tp
+// #4a Created folders and files to auth/sign-up. Go there.
+
+router.post("/sign-up", async (req, res) => {
+    const userInDatabase = await User.findOne({ username: req.body.username });
+        if (userInDatabase) {
+            return res.send("Username already taken.");
+        }
+        if (req.body.password !== req.body.confirmPassword) {
+            return res.send("Password and Confirm Password must match");
+        }
+        const hashedPassword = bcrypt.hashSync(req.body.password, 10); // <- This creates the new Hased pw and is put in a string
+            req.body.password = hashedPassword; // <- We are now making it so that the password and hashed version are the same. 
+            
+            const user = await User.create(req.body);
+            res.send(`Thanks for signing up ${user.username}`);
+            
+        req.session.user = {
+            username: user.username,
+        };
+              
+        req.session.save(() => {
+            res.redirect("/");
+        });
+              
+});
+  
+// #6 Created the Post route for signup. Imported user into a variable
+// #6a Creating Authorization to have a unique Username and same password
+
+// #7 Installing bcrypt. Putting in security safety. That is a hashing package.
+
+router.get("/sign-in", (req, res) => {
+    res.render("auth/sign-in.ejs");
+});
+
+// #8 Created GET Sign-in page 
+
+router.post("/sign-in", async (req, res) => {
+    // Get user from Database
+    const userInDatabase = await User.findOne({ username: req.body.username });
+    if (!userInDatabase) {
+        return res.send("Login failed. Please try again.");
+    } 
+    // There is a User! Time t otest their PW w/ bcrypt
+    const validPassword = bcrypt.compareSync(
+        req.body.password,
+        userInDatabase.password
+      );
+      if (!validPassword) {
+        return res.send("Login failed. Please try again.");
+      }
+          
+    // There is a user AND they had the correct password. Time to make a session!
+    // Avoid storing the password, even in hashed format, in the session
+    // If there is other data you want to save to `req.session.user`, do so here!
+    req.session.user = { 
+        username: userInDatabase.username,
+        // _id: userInDatabase._id
+    } // #9a Installed express-session and created some validation.
+
+    req.session.save(() => {
+        res.redirect("/");
+});
+
+
+router.get("/sign-out", async (req, res) => {
+    req.session.destroy(() => {
+        res.redirect("/");
+      });
+    });
+});
+  
+// #12 Created a Sign-out route. To close a session(Being Signed-in) is to delete/destroy session
+// Right now this literally destroys the user as well. Will fix later
+
+  
+// #9 Created POST Sign-in.
+
+module.exports = router;
+
--- server.js
+++ server.js
@@ -7,12 +7,47 @@ const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
 const authController = require("./controllers/auth.js");
+const session = require('express-session');
+const MongoStore = require("connect-mongo");
+
 
 const PORT = process.env.PORT ? process.env.PORT : "3000"; // Set the port from environment variable or default to 3000
 
 app.use(express.urlencoded({ extended: false })); // Middleware to parse URL-encoded data from forms
 app.use(methodOverride("_method")); // Middleware for using HTTP verbs such as PUT or DELETE
 app.use(morgan('dev')); // Morgan for logging HTTP requests
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
+    })
+);
+
+//ROUTES BELOW
+app.use("/auth", authController); // #4 Prepends the routes
+
+app.get("/", async (req, res) => { // #2 Created Landing page and made a views folder. Create index.ejs file inside w/ HTML.
+    res.render("index.ejs", {
+      user: req.session.user, //#10 Linked this to the sessions made in controllers/auth.js
+    });
+});
+  
+  // #3 Next Make a User Model. Make model folder. Make user.js. Go there.
+  // #4 Create a controllers folder. Create a auth.js inside. Go there.
+
+
+app.get("/vip-lounge", (req, res) => {
+    if (req.session.user) {
+      res.send(`Welcome to the party ${req.session.user.username}.`);
+    } else {
+      res.send("Sorry, no guests allowed.");
+    }
+});
+// #13 Created a resticted route where only validated users can be. VIP Lounge
 
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on("connected", () => {
@@ -22,13 +57,3 @@ mongoose.connection.on("connected", () => {
         console.log(`The express app is ready on PORT ${PORT}!`);
     });
 });
-
-//ROUTES BELOW
-app.use("/auth", authController); // #4 Prepends the routes
-
-app.get("/", async (req, res) => { // #2 Created Landing page and made a views folder. Create index.ejs file inside w/ HTML.
-    res.render("index.ejs");
-  });
-  
-  // #3 Next Make a User Model. Make model folder. Make user.js. Go there.
-  // #4 Create a controllers folder. Create a auth.js inside. Go there.
